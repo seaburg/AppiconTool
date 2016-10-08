ccae01632f5e719caa662b320041f42cec83c3bd
Evgeniy Yurtaev <evgeniyyurt@gmail.com>
2016-10-08 12:39:32 +0300

    Initial Commit

---
+++ AppiconText/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AppiconText
+//
+//  Created by Evgeniy Yurtaev on 08/10/2016.
+//  Copyright © 2016 Evgeniy Yurtaev. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

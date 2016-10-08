15eb857be9ab69e2512f6b5aeccacc756fb6ea19
Evgeniy Yurtaev <evgeniyyurt@gmail.com>
2016-10-08 23:18:26 +0300

    Create icon processing protocol

---
+++ AppiconText/IconProcessors/IconProcessing.swift
@@ -0,0 +1,13 @@
+//
+//  IconProcessing.swift
+//  AppiconText
+//
+//  Created by Evgeniy Yurtaev on 08/10/2016.
+//  Copyright © 2016 Evgeniy Yurtaev. All rights reserved.
+//
+
+import AppKit
+
+public protocol IconProcessing {
+    func process(_ icon: NSImage, contentScale: CGFloat) throws -> NSImage
+}

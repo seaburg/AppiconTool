fbd5970ed86eb22cf2b8cad7f5c42f3a7cc1d08e
Evgeniy Yurtaev <evgeniyyurt@gmail.com>
2016-10-09 18:28:26 +0300

    Create method for convertation string to color

--- AppiconText/NSColorExtension.swift
+++ AppiconText/NSColorExtension.swift
@@ -9,6 +9,42 @@
 import AppKit
 
 public extension NSColor {
+    public convenience init(number: UInt, hasAlpha: Bool = false) {
+        let red, green, blue, alpha: CGFloat
+
+        var number = number
+        if hasAlpha {
+            alpha = CGFloat(number & 0xff) / 255
+            number = number >> 8
+        } else {
+            alpha = 1
+        }
+        blue = CGFloat(number & 0xff) / 255
+        number = number >> 8
+
+        green = CGFloat(number & 0xff) / 255
+        number = number >> 8
+
+        red = CGFloat(number & 0xff) / 255
+        number = number >> 8
+
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+
+    public convenience init?(hex string: String) {
+        var canonicalString = string.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).lowercased()
+
+        if canonicalString.hasPrefix("0x") || canonicalString.hasPrefix("x") {
+            canonicalString = string.substring(from: string.range(of: "x")!.upperBound)
+        }
+        let hasAlphaComponent = (canonicalString.characters.count > 6)
+
+        guard let number = UInt(canonicalString, radix: 16) else {
+            return nil
+        }
+        self.init(number: number, hasAlpha: hasAlphaComponent)
+    }
+
     public var luminance: CGFloat {
         guard let components = self.cgColor.components else {
             return 0
